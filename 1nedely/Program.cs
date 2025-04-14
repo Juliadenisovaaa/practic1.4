ca0bfbb18af6e38bf5b3bc74b9b75ff3c69162db
Juliadenisovaaa <denisovajyl@gmail.com>
2025-04-14 10:07:11 +0200

    Добавьте файлы проекта.

---
+++ 1nedely/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1nedely
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string GetInfo() 
+        {
+            return $"Название: {Name}, Цена: {Price}";
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine(GetInfo());
+        }
+    }
+
+
+    public class Electronics : Product
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Electronics(string name, double price, string brand, string model) : base(name, price) 
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public override string GetInfo() 
+        {
+            return $"{base.GetInfo()}, Бренд: {Brand}, Модель: {Model}";
+        }
+
+        public override void PrintInfo() 
+        {
+            Console.WriteLine(GetInfo());
+        }
+    }
+
+
+    public class Store
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void PrintAllProducts()
+        {
+            foreach (var product in Products)
+            {
+                product.PrintInfo();
+                Console.WriteLine("---"); 
+            }
+        }
+    }
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+
+            Store store = new Store();
+
+
+            Product laptop = new Product("Ноутбук", 34000.00);
+            Electronics phone = new Electronics("Смартфон", 76000.00, "Samsung", "Galaxy S23");
+
+            store.AddProduct(laptop);
+            store.AddProduct(phone);
+
+            Console.WriteLine("Товары в магазине:");
+            store.PrintAllProducts();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1nedely/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("1nedely")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1nedely")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("122a5de7-e5e4-4b33-adc4-4c5dee0524fd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
